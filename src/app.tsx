169343b7896296357b3da1270eb79d308e023cd5
dar-ksen <ksen.vl.set@gmail.com>
2023-07-19 20:14:01 +0300

    feat(app) add theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,32 @@
-import Counter from './components/counter';
-import './index.scss';
+import { Suspense, useContext } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import cn from 'classnames';
+
+import './styles/index.scss';
+import { ThemeContext } from './theme/ThemeContext';
+import { useTheme } from './theme/useTheme';
+
+export enum Theme {
+  LIGHT = 'light',
+  DARK = 'dark'
+}
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme()
+
   return (
-    <div className='app'>
-      appTitle
-      <Counter/>
+    <div className={ cn('app', theme) }>
+      <button onClick={ toggleTheme }>TOGGLE</button>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageLazy />}/>
+          <Route path={'/'} element={<MainPageLazy />}/>
+        </Routes>
+      </Suspense>
     </div>
   );
 };
